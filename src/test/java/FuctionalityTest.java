3dee5ebf83dd2d57609ac76d4ed0b9fb09bd5ff0
snvkez <a.b.elous@mail.ru>
2023-01-20 19:00:19 +0400

    init

---
+++ src/test/java/FuctionalityTest.java
@@ -0,0 +1,58 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.TapOptions;
+import io.appium.java_client.touch.offset.ElementOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FuctionalityTest {
+
+    static AppiumDriver<MobileElement> driver;
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "192.168.85.105:5555");
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android"); //platformName
+        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9"); //platformVersion
+        cap.setCapability(MobileCapabilityType.UDID, "192.168.85.105:5555");
+
+        cap.setCapability("appPackage", "com.android.gallery3d");
+        cap.setCapability("appActivity","com.android.gallery3d.app.GalleryActivity");
+
+        AppiumDriver<MobileElement> driver = new AppiumDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        TouchAction touchAction = new TouchAction(driver);
+
+
+        System.out.println(driver.findElementById("android:id/text1").getTagName());
+
+
+      /*
+
+
+        String[] numpad = {
+                "com.android.calculator2:id/digit_4",
+                "com.android.calculator2:id/op_mul",
+                "com.android.calculator2:id/digit_5",
+                "com.android.calculator2:id/eq"};
+
+
+        for(int i=0;i<numpad.length;i++)
+            touchAction.tap(TapOptions.tapOptions()
+                            .withElement(ElementOption.element(driver.findElement(By
+                                    .id(numpad[i])))))
+                    .perform();
+
+        assertEquals("Not true result","20",driver.findElementById("com.android.calculator2:id/result").getText());
+
+*/
+
+        driver.closeApp();
+    }
+}
